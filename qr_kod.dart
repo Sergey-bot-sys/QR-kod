1c61eb244dd12af7f80ce68e7e3735cc5300b613
Sergey-bot-sys <79085670+Sergey-bot-sys@users.noreply.github.com>
2022-12-23 21:52:59 +0300

    Update qr_kod.dart
    Добавлены комментарии что нужно вписать в кавычки.

--- qr_kod.dart
+++ qr_kod.dart
@@ -25,12 +25,14 @@ class _qr_kodState extends State<qr_kod> {
         body: Container(
     decoration: BoxDecoration(
       image: DecorationImage(
+        //В кавычках указывается имя файла на котором изображено фото документа
         image: AssetImage("assets/kotik-012b2.jpg"),
         fit: BoxFit.cover,
         ),
       ),
           child:
           PhotoView(
+            //В кавычках указывается имя файла на котором изображено фото документа
             imageProvider: AssetImage("assets/kotik-012b2.jpg"),
           )
         ),
